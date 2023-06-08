a028aa0700ddbe5edde33b0a6904cd93d1deb7c5
christopher pellegrini <106196537+mrtoaf@users.noreply.github.com>
2023-06-08 14:02:19 -0400

    function, loading, viewing!

--- iris.py
+++ iris.py
@@ -3,5 +3,12 @@ import numpy as np
 import pandas as pd
 from sklearn import datasets
 
-iris = datasets.load_iris()
-iris.keys()
+
+def load_data():
+    data = load_iris(as_frame=True)
+    iris_dataframe = data.as_frame
+    return iris_dataframe
+
+
+if __name__ == "__main__":
+    load_data.info()
