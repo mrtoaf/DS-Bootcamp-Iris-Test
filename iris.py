c2b720a1392416be20779f00e48cf680e719dd57
christopher pellegrini <106196537+mrtoaf@users.noreply.github.com>
2023-06-08 14:04:47 -0400

    printed out dataset

--- iris.py
+++ iris.py
@@ -2,13 +2,15 @@
 import numpy as np
 import pandas as pd
 from sklearn import datasets
+from sklearn.datasets import load_iris
 
 
 def load_data():
     data = load_iris(as_frame=True)
-    iris_dataframe = data.as_frame
+    iris_dataframe = data.frame
     return iris_dataframe
 
 
 if __name__ == "__main__":
-    load_data.info()
+    df_data = load_data()
+    df_data.info()
