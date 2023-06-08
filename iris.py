3094cb69aef90ee07715d67261d95f5e0fe6d519
ashleyjeon <112719138+ashleyjeon@users.noreply.github.com>
2023-06-08 14:13:10 -0400

    split function

--- iris.py
+++ iris.py
@@ -2,12 +2,16 @@ import numpy as np
 import pandas as pd
 from sklearn import datasets
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 
 def load_data():
     data = load_iris(as_frame=True)
     iris_dataframe = data.frame
     return iris_dataframe
+
+def split_data():
+    
 
 # 0 is Setosa
 # 1 is Versicolor
@@ -16,3 +20,5 @@ def load_data():
 if __name__ == "__main__":
     df_data = load_data()
     df_data.info()
+
+
